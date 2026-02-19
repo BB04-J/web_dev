e0233db59fdfbf14f90721645c77944a1922504c
Bhakti Bhanushali <bhaktibhanushali000@gmail.com>
2026-02-20 02:37:29 +0530

    Add files via upload

---
+++ ass2_2.js
@@ -0,0 +1,15 @@
+//Write code to read contents of a file and print it to the console. You can use the fs library to as a black box, the goal is to understand async tasks. Try to do an expensive operation below the file read and see how it affects the output. Make the expensive operation more and more expensive and see how it affects the output.
+
+
+// const fs = require("fs");    
+import fs from "fs";
+
+fs.readFile("a.txt","utf-8", function(err,data){
+    console.log("file content bellow");
+    console.log(data);
+});
+let j=0;
+for(let i=0;i<=100000;i++){
+    j++;
+}
+console.log("The expensive task is completed = " + j)
